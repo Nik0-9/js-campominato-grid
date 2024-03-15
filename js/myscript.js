10ca588c24bd271cb9b0b09910e54603137e2fb2
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-15 17:29:21 +0100

    create function generateCell that create element div with class cell. create const gridEl for add class at element selected

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,25 @@
-let btnStart = document.querySelector('btn-outline-dark')
+let btnStart = document.querySelector('.btn-outline-dark')
 
 btnStart.addEventListener('click', function(){
 
-})
+const gridEl = document.getElementById('grid');
+gridEl.innerHTML = '';
+//console.log(gridEl);
+gridEl.classList.add('d-flex', 'flex-wrap');
+//console.log(gridEl);
+
+ for (i = 0; i < 100; i++) {
+    gridEl.appendChild(generateCell(i +1));
+ }
+});
+
+
+function generateCell(content) {
+    let addCell = document.createElement('div');
+    addCell.innerHTML = content;
+    //console.log(addCell);
+    addCell.classList.add('cell');
+    return addCell;
+}
+
 
