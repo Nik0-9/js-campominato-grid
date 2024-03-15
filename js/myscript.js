f1b7956fb9cc1a67f3ccdf8762b18072312b8ffd
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-15 20:16:01 +0100

    add condition for generate less cell for the other difficult

--- js/myscript.js
+++ js/myscript.js
@@ -1,22 +1,47 @@
-let btnStart = document.querySelector('.btn-outline-dark')
+let btnStart = document.querySelector('.btn-outline-dark');
 
 btnStart.addEventListener('click', function(){
-
+    
+let difficulty = document.getElementById('difficulty').value;
 const gridEl = document.getElementById('grid');
 gridEl.innerHTML = '';
 //console.log(gridEl);
 gridEl.classList.add('d-flex', 'flex-wrap');
 //console.log(gridEl);
-
- for (i = 0; i < 100; i++) {
-    const newCell = generateCell(i +1);
-    newCell.addEventListener('click', function(){
-        newCell.classList.toggle('clicked');
-    })
-    gridEl.appendChild(newCell);
- }
+if(difficulty === '1'){
+    console.log(difficulty);
+    for (i = 0; i < 100; i++) {
+        const newCell = generateCell(i + 1);
+        gridEl.appendChild(newCell);
+        newCell.addEventListener('click', function(){
+            newCell.classList.toggle('clicked');
+            console.log(newCell.innerHTML);
+        });
+    }
+} else if(difficulty === '2'){
+    console.log(difficulty);
+    for (i = 0; i < 81; i++) {
+        const newCell = generateCell(i + 1);
+        gridEl.appendChild(newCell);
+        newCell.classList.add('cell-medium');
+        newCell.addEventListener('click', function(){
+            newCell.classList.toggle('clicked');
+            console.log(newCell.innerHTML);
+        });
+    }
+} else if(difficulty === '3'){
+    console.log(difficulty);
+    for (i = 0; i < 49; i++) {
+        const newCell = generateCell(i + 1);
+        gridEl.appendChild(newCell);
+        newCell.classList.add('cell-hard');
+        newCell.addEventListener('click', function(){
+            newCell.classList.toggle('clicked');
+            console.log(newCell.innerHTML);
+        });
+    }
+}
 });
-
 
 function generateCell(content) {
     let addCell = document.createElement('div');
@@ -25,3 +50,5 @@ function generateCell(content) {
     addCell.classList.add('cell');
     return addCell;
 }
+
+
