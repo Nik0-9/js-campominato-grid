2fc8be37e1920ec5b5077e421ec6bb31f73bea13
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-16 20:27:47 +0100

    create function getCellsByDifficulty and function play

--- js/myscript.js
+++ js/myscript.js
@@ -1,54 +1,55 @@
 let btnStart = document.querySelector('.btn-outline-dark');
 
-btnStart.addEventListener('click', function(){
+btnStart.addEventListener('click', function () {
     
-let difficulty = document.getElementById('difficulty').value;
-const gridEl = document.getElementById('grid');
-gridEl.innerHTML = '';
-//console.log(gridEl);
-gridEl.classList.add('d-flex', 'flex-wrap');
-//console.log(gridEl);
-if(difficulty === '1'){
-    console.log(difficulty);
-    for (i = 0; i < 100; i++) {
-        const newCell = generateCell(i + 1);
-        gridEl.appendChild(newCell);
-        newCell.addEventListener('click', function(){
-            newCell.classList.toggle('clicked');
-            console.log(newCell.innerHTML);
-        });
-    }
-} else if(difficulty === '2'){
-    console.log(difficulty);
-    for (i = 0; i < 81; i++) {
-        const newCell = generateCell(i + 1);
-        gridEl.appendChild(newCell);
-        newCell.classList.add('cell-medium');
-        newCell.addEventListener('click', function(){
-            newCell.classList.toggle('clicked');
-            console.log(newCell.innerHTML);
-        });
-    }
-} else if(difficulty === '3'){
-    console.log(difficulty);
-    for (i = 0; i < 49; i++) {
-        const newCell = generateCell(i + 1);
-        gridEl.appendChild(newCell);
-        newCell.classList.add('cell-hard');
-        newCell.addEventListener('click', function(){
-            newCell.classList.toggle('clicked');
-            console.log(newCell.innerHTML);
-        });
-    }
-}
+    let difficulty = document.getElementById('difficulty').value;
+    const mainEl = document.querySelector('main');
+    const sectionEl = document.createElement('section');
+    // newGrid.innerHTML = '';
+    sectionEl.id ='grid';
+    mainEl.innerHTML ='';
+    mainEl.appendChild(sectionEl);
+    // gridEl.innerHTML = '';
+    //console.log(gridEl);
+    const gridEl = document.getElementById('grid');
+    gridEl.classList.add('d-flex', 'flex-wrap');
+    //console.log(gridEl);
+    play(difficulty, gridEl);
 });
 
-function generateCell(content) {
+function getCellsByDifficulty(difficulty){
+    if(difficulty === 'easy'){
+        return 100;
+    }else if (difficulty === 'medium'){
+        return 81;
+    }else if(difficulty === 'hard'){
+        return 49;
+    }
+}
+
+function generateCell(content, difficulty) {
+
     let addCell = document.createElement('div');
     addCell.innerHTML = content;
     //console.log(addCell);
     addCell.classList.add('cell');
+    if(difficulty !== 'easy'){
+        addCell.classList.add(`cell-${difficulty}`);
+    }
     return addCell;
+}
+
+function play(difficulty, grid){
+    const cells = getCellsByDifficulty(difficulty);
+    console.log(cells);
+    for (i = 0; i < cells ; i++){
+        const newCell = generateCell(i + 1, difficulty);
+        grid.appendChild(newCell);
+        newCell.addEventListener('click', function () {
+            newCell.classList.toggle('clicked');
+            console.log(newCell.innerHTML);
+        });
+    }
 }
 
 
